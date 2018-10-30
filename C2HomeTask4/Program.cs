41815f741cf65b863655647f7ae84908eabb5aa3
DonVolkRam <success.vka@gmail.com>
2018-10-30 14:54:27 +0300

    Выполнено 3 задание

--- C2HomeTask4/Program.cs
+++ C2HomeTask4/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Reflection;
 
-namespace C2HomeTask4
+namespace Ex2
 {
-    class Program
+    static class Program
     {
+        public static Dictionary<T, int> CountElements<T>(this List<T> self)
+        {
+            Dictionary<T, int> elements = new Dictionary<T, int>();
+            elements.Add(self[0], 1);
+            for (int i = 1; i < self?.Count; i++)
+            {               
+                for (int j = 0; j < elements?.Count; j++)
+                {
+                    if (elements[j].)
+                }
+            }
+            return elements;
+        }
+
         static void Main(string[] args)
         {
+            List<int> elements = new List<int>();
+            Random rnd = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                elements.Add(rnd.Next(0, 11));
+            }
         }
     }
 }
---
+++ Ex3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>()
+            {
+                { "four" , 4 },
+                { "two" , 2 },
+                { "one" , 1 },
+                { "three" , 3 },
+            };
+            //необходимо раскоментарить для проверки
+            //Задача 3
+            //Исхожный текст
+            //var d = dict.OrderBy(delegate (KeyValuePair<string, int> pair) { return pair.Value; });
+
+            //задание 3.а
+            //var d = dict.OrderBy(pair => pair.Value);
+
+            //задание 3.b
+            int GetValue(KeyValuePair<string, int> D) { return D.Value; }
+            var d = dict.OrderBy(GetValue);
+
+            foreach (var pair in d)
+            {
+                Console.WriteLine("{0} - {1}", pair.Key, pair.Value);
+            }
+
+            Console.Read();
+        }
+    }
+}
